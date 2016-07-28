a3192f548005d50db59602ee62bdf9c06bb351f4
cahhbwy <cahhbwy@mail.ustc.edu.cn>
2016-07-28 21:30:32 +0800

    011 AC 56.88%

--- 011_Container With Most Water/011_Container With Most Water.cpp
+++ 011_Container With Most Water/011_Container With Most Water.cpp
@@ -5,23 +5,36 @@ class Solution {
 public:
 	int maxArea(vector<int>& height) {
 		int length = height.size();
-		int maxAreaResult = 0, areaTemp;
+		int leftMH = 0, rightMH, tempA, maxA = 0;
 		for (int i = 0; i < length; ++i) {
-			for (int j = i; j < length; ++j) {
-				areaTemp = (j - i)*(height[i] < height[j] ? height[i] : height[j]);
-				maxAreaResult = maxAreaResult > areaTemp ? maxAreaResult : areaTemp;
+			if (height[i] > leftMH) {
+				leftMH = height[i];
+				rightMH = 0;
+				for (int j = length - 1; j > i; --j) {
+					if (height[j] > rightMH) {
+						rightMH = height[j];
+						if (leftMH < rightMH) {
+							tempA = leftMH*(j - i);
+							maxA = maxA > tempA ? maxA : tempA;
+							break;
+						} else {
+							tempA = rightMH*(j - i);
+							maxA = maxA > tempA ? maxA : tempA;
+						}
+					}
+				}
 			}
 		}
-		return maxAreaResult;
+		return maxA;
 	}
 };
 int main(int argc, char** argv) {
 	Solution solution;
-	vector<int> height;
-	height.resize(10000);
-	for (int i = 0; i < 10000; ++i) {
-		height[i] = rand() & 0xff;
-	}
+	vector<int> height = { 1, 1 };
+	// 	height.resize(10000);
+	// 	for (int i = 0; i < 10000; ++i) {
+	// 		height[i] = rand() & 0xff;
+	// 	}
 	cout << solution.maxArea(height) << endl;
 	return 0;
 }
