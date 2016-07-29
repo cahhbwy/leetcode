eed2a0d68ee17d40441f80a63f2d94b0003cf4d3
cahhbwy <cahhbwy@mail.ustc.edu.cn>
2016-07-29 09:00:31 +0800

    014 AC 53.44%

--- 014_Longest Common Prefix/014_Longest Common Prefix.cpp
+++ 014_Longest Common Prefix/014_Longest Common Prefix.cpp
@@ -7,18 +7,22 @@ class Solution {
 public:
 	string longestCommonPrefix(vector<string>& strs) {
 		if (strs.empty())	return "";
-		sort(strs.begin(), strs.end());
-		const char *c1 = strs.front().c_str(), *c2 = strs.back().c_str();
+		string maxStr = strs.front(), minStr = strs.front();
+		for (string s : strs) {
+			maxStr = maxStr > s ? maxStr : s;
+			minStr = minStr < s ? minStr : s;
+		}
+		const char *c1 = maxStr.c_str(), *c2 = minStr.c_str();
 		while (*c1 == *c2&&*c1 != 0) {
 			++c1;
 			++c2;
 		}
-		return strs.front().substr(0, c1 - strs.front().c_str());
+		return maxStr.substr(0, c1 - maxStr.c_str());
 	}
 };
 int main(int argc, char** argv) {
 	Solution solution;
-	vector<string> strs = {  };
+	vector<string> strs = {};
 	cout << solution.longestCommonPrefix(strs) << endl;
 	return 0;
 }
