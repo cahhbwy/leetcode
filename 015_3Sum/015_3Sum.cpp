4b663b7ed2b6c94e0b9b9fa1da3adedcc3776ada
cahhbwy <cahhbwy@mail.ustc.edu.cn>
2016-07-30 15:14:35 +0800

    015 AC 39.84%

--- 015_3Sum/015_3Sum.cpp
+++ 015_3Sum/015_3Sum.cpp
@@ -16,44 +16,63 @@ public:
 			if (i > 0 && nums[i] == nums[i - 1]) {
 				continue;
 			}
-			for (int j = i + 1; j < size - 1; ++j) {
-				if (j - 1 > i&&nums[j] == nums[j - 1]) {
-					continue;
-				}
-				int head = j + 1, tail = size - 1, k, f = -nums[i] - nums[j];
-				if (nums[head] <= f&&nums[tail] >= f) {
-					while (head < tail - 1) {
-						k = (head + tail) / 2;
-						if (nums[k] < f) {
-							head = k;
-						} else {
-							tail = k;
-						}
-					}
-					if (nums[head] == f) {
-						resultTemp[0] = nums[i];
-						resultTemp[1] = nums[j];
-						resultTemp[2] = nums[head];
-						if (result.empty() || result.back()[0] != resultTemp[0] || result.back()[1] != resultTemp[1] || result.back()[2] != resultTemp[2]) {
-							result.push_back(resultTemp);
-						}
-					} else if (nums[tail] == f) {
-						resultTemp[0] = nums[i];
-						resultTemp[1] = nums[j];
-						resultTemp[2] = nums[tail];
-						if (result.empty() || result.back()[0] != resultTemp[0] || result.back()[1] != resultTemp[1] || result.back()[2] != resultTemp[2]) {
-							result.push_back(resultTemp);
-						}
-					}
+			int head = i + 1, tail = size - 1, f = -nums[i];
+			while (head < tail) {
+				if (nums[head] + nums[tail]>f) {
+					--tail;
+				} else if (nums[head] + nums[tail] < f) {
+					++head;
+				} else {
+					resultTemp[0] = nums[i];
+					resultTemp[1] = nums[head];
+					resultTemp[2] = nums[tail];
+					result.push_back(resultTemp);
+					do {
+						++head;
+					} while (head < tail&&nums[head] == resultTemp[1]);
+					do {
+						--tail;
+					} while (tail > head&&nums[tail] == resultTemp[2]);
 				}
 			}
+			// 			for (int j = i + 1; j < size - 1; ++j) {
+			// 				if (j - 1 > i&&nums[j] == nums[j - 1]) {
+			// 					continue;
+			// 				}
+			// 				int head = j + 1, tail = size - 1, k, f = -nums[i] - nums[j];
+			// 				if (nums[head] <= f&&nums[tail] >= f) {
+			// 					while (head < tail - 1) {
+			// 						k = (head + tail) / 2;
+			// 						if (nums[k] < f) {
+			// 							head = k;
+			// 						} else {
+			// 							tail = k;
+			// 						}
+			// 					}
+			// 					if (nums[head] == f) {
+			// 						resultTemp[0] = nums[i];
+			// 						resultTemp[1] = nums[j];
+			// 						resultTemp[2] = nums[head];
+			// 						if (result.empty() || result.back()[0] != resultTemp[0] || result.back()[1] != resultTemp[1] || result.back()[2] != resultTemp[2]) {
+			// 							result.push_back(resultTemp);
+			// 						}
+			// 					} else if (nums[tail] == f) {
+			// 						resultTemp[0] = nums[i];
+			// 						resultTemp[1] = nums[j];
+			// 						resultTemp[2] = nums[tail];
+			// 						if (result.empty() || result.back()[0] != resultTemp[0] || result.back()[1] != resultTemp[1] || result.back()[2] != resultTemp[2]) {
+			// 							result.push_back(resultTemp);
+			// 						}
+			// 					}
+			// 				}
+			// 			}
 		}
 		return result;
 	}
 };
 int main(int argc, char** argv) {
 	Solution solution;
-	vector<int> nums = { -1, 0, 1, 2, -1, -4 };
+	vector<int> nums = { -2, 0, 0, 2, 2};
 	vector<vector<int>> result = solution.threeSum(nums);
 	for (auto i : result) {
 		cout << "[";
