71cd917398f696f02b5a04cf4c814fe6ace53b4f
cahhbwy <cahhbwy@mail.ustc.edu.cn>
2016-07-31 09:32:11 +0800

    021 AC 9.37%

--- 021_Merge Two Sorted Lists/021_Merge Two Sorted Lists.cpp
+++ 021_Merge Two Sorted Lists/021_Merge Two Sorted Lists.cpp
@@ -8,13 +8,45 @@ struct ListNode {
 class Solution {
 public:
 	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-		
+		if (l1&&!l2) {
+			return l1;
+		} else if (!l1&&l2) {
+			return l2;
+		} else if (!l1&&!l2) {
+			return NULL;
+		}
+		ListNode *l, *p;
+		if (l1->val < l2->val) {
+			l = l1;
+			l1 = l1->next;
+		} else {
+			l = l2;
+			l2 = l2->next;
+		}
+		p = l;
+		while (l1&&l2) {
+			if (l1->val < l2->val) {
+				p->next = l1;
+				l1 = l1->next;
+			} else {
+				p->next = l2;
+				l2 = l2->next;
+			}
+			p = p->next;
+		}
+		if (l1&&!l2) {
+			p->next = l1;
+		} else {
+			p->next = l2;
+		}
+		return l;
 	}
 	ListNode* generate(int size) {
-		ListNode *head = new ListNode(rand() % 10);
+		if (size <= 0) return NULL;
+		ListNode *head = new ListNode(rand() % 3);
 		ListNode *p = head;
 		while (--size > 0) {
-			p->next = new ListNode(rand() % 10);
+			p->next = new ListNode(1 + p->val + rand() % 3);
 			p = p->next;
 		}
 		return head;
@@ -30,7 +62,7 @@ public:
 };
 int main(int argc, char **argv) {
 	Solution solution;
-	ListNode *l1 = solution.generate(5);
+	ListNode *l1 = solution.generate(0);
 	ListNode *l2 = solution.generate(7);
 	solution.output(l1);
 	solution.output(l2);
