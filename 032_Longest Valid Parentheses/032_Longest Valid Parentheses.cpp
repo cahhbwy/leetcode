289c4760c2da1a8728b950dfd386d68bd0fa79dd
cahhbwy <cahhbwy@mail.ustc.edu.cn>
2017-09-10 11:10:21 +0800

    032 34.51%

--- 032_Longest Valid Parentheses/032_Longest Valid Parentheses.cpp
+++ 032_Longest Valid Parentheses/032_Longest Valid Parentheses.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 class Solution {
 public:
+	/*
 	struct Node
 	{
 		unsigned int i;
@@ -14,16 +15,18 @@ public:
 	int longestValidParentheses(string s) {
 		vector<Node> nodes;
 		int result = 0;
+		nodes.push_back(Node(0, ')'));
 		for (unsigned int i = 0; i < s.length(); ++i) {
-			nodes.push_back(Node(i, s[i]));
+			nodes.push_back(Node(i + 1, s[i]));
 		}
+		nodes.push_back(Node(s.length() + 1, '('));
 		unsigned int a = 0, b = 1;
 		while (b < nodes.size()) {
 			if (nodes[a].c == '('&&nodes[b].c == ')') {
 				nodes.erase(nodes.begin() + b);
 				nodes.erase(nodes.begin() + a);
-				a = a - 1 >= 0 ? a - 1 : 0;
-				b = a + 1;
+				--a;
+				--b;
 			}
 			else {
 				++a;
@@ -33,15 +36,46 @@ public:
 		for (int i = 0, j = 1; j < nodes.size(); ++i, ++j) {
 			result = result > (nodes[j].i - nodes[i].i - 1) ? result : (nodes[j].i - nodes[i].i - 1);
 		}
-		if (nodes.size() < 2) {
-			return s.length() - nodes.size();
+		return result;
+	}
+	*/
+	int longestValidParentheses(string s) {
+		int length = s.length() + 2;
+		char *temp = new char[length + 1];
+		temp[0] = ')';
+		strncpy(temp + 1, s.c_str(), s.length());
+		temp[length - 1] = '(';
+		temp[length] = 0;
+		int a = 0, b = 1;
+		while (b < length) {
+			if (temp[a] == '('&&temp[b] == ')') {
+				temp[a] = temp[b] = '_';
+				while (temp[--a] == '_');
+				++b;
+			}
+			else {
+				a = b;
+				++b;
+			}
+		}
+		int result = 0, tmp = 0;
+		char *c = temp;
+		while (*c) {
+			if (*c == '_') {
+				++tmp;
+			}
+			else {
+				result = result < tmp ? tmp : result;
+				tmp = 0;
+			}
+			++c;
 		}
 		return result;
 	}
 };
 int main(int argc, char **argv) {
 	Solution solution;
-	cout << solution.longestValidParentheses("()(())") << endl;
+	cout << solution.longestValidParentheses("(()())") << endl;
 	system("pause");
 	return 0;
 }
